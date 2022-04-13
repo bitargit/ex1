8b552fd40f1bd8d5d0bef122f7cb1954af58f0c6
bitargit <102753903+bitargit@users.noreply.github.com>
2022-04-13 20:21:46 +0300

    Add files via upload

--- RLEList.c
+++ RLEList.c
@@ -1,7 +1,60 @@
+//
+// Created by USER on 13/04/2022.
+//
 #include "RLEList.h"
 
-struct RLEList_t{
-    //TODO: implement
-};
+typedef struct RLEList_t{
+    int numberOfCharachters;
+    char character;
+    struct RLEList_t* next;
+} *RLEList;
 
-//implement the functions here
+
+RLEList RLEListCreate()
+{
+    RLEList ptr = malloc(sizeof(*ptr));
+    if(!ptr)
+    {
+        return NULL;
+    }
+}
+
+void RLEListDestroy(RLEList list)
+{
+    while (list)
+    {
+        RLEList toDelete = list;
+        list = list->next;
+        free(toDelete);
+    }
+}
+
+RLEListResult RLEListAppend(RLEList list, char value)
+{
+    RLEList current = list;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    if (current->character == value)
+    {
+        current->numberOfCharachters++;
+    }
+    current->next = malloc(sizeof(RLEList));
+    current->next->character = value;
+    current->next->numberOfCharachters = 0;
+}
+
+int RLEListSize(RLEList list)
+{
+    int total = 0;
+    RLEList current = list;
+    while (current->next != NULL)
+    {
+        total += current->numberOfCharachters;
+        current = current->next;
+    }
+    return total;
+}
+
+
