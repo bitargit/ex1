1f2d3b29f70c43c031a9248a7e9bf8a4b34a6073
Maher Bitar <maherbitar@campus.technion.ac.il>
2022-04-27 22:09:43 +0300

    PrintEncoded is added, asciiArtPrint is modified

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -11,17 +11,11 @@
 RLEList asciiArtRead(FILE* in_stream) {
     RLEList list = RLEListCreate();
     RLEList current = list;
-//    FILE* in_str = fopen(in_stream, 'r');
-//    if (!in_str) {
-//        //error
-//        return 0;
-//    }
 
-    char buffer[1] = "";
-
-    while (*buffer != EOF){//OR buffer[0]
-        fgets(buffer, 1, in_stream);
-        RLEListAppend(current, buffer[0]);
+    char character;
+    while ((character = fgetc(in_stream)) != EOF){
+        printf("%c\n", character);
+        RLEListAppend(current, character);
         //in_str++;
     }
     fclose(in_stream);
@@ -32,21 +26,33 @@ RLEList asciiArtRead(FILE* in_stream) {
 
 RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
 {
-    RLEListResult *result = NULL;
     int list_len = RLEListSize(list);
-    for (int index; index < list_len; index++){
-        char current_character = RLEListGet(list, index, result);
-        fputs(current_character, out_stream);
+    for (int index = 0; index < list_len; index++){//OR index = 1
+        char current_character = RLEListGet(list, index, NULL);
+        fputc(current_character, out_stream);
     }
 
-    return result;
+    return RLE_LIST_SUCCESS;
 }
 
 
 
 RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream)
 {
+    if (list == NULL || out_stream == NULL){
+        return RLE_LIST_NULL_ARGUMENT;
+    }
 
+    char *stringExported = RLEListExportToString(list, NULL);
+    printf("exported =\n%s\n", stringExported);
+    int i = 0;
+    while(stringExported[i] != '\0'){
+        char current_character = stringExported[i];
+        fputc(current_character, out_stream);
+        i++;
+    }
+
+    return RLE_LIST_SUCCESS;
 }
 
 
